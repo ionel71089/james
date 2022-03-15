6c5d2d8b380be2fc00a1197b661d9164df10e3f1
ionel71089 <ionel.lescai@flir.com>
2022-03-15 13:56:06 +0200

    add script

--- .vscode/transform.sh
+++ .vscode/transform.sh
@@ -2,5 +2,6 @@
 
 saxon -s:input.chlsx -xsl:transform.xslt -o:output.xml
 cat output.xml | xq . > output.json
+./json-to-csv output.json output.csv
 
 # saxon -s:input.chlsx -xsl:transform.xslt | xq . > output.json
